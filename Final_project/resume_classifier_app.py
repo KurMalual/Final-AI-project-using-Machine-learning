bfe456947ea80fe99d4aa17048cd16c92591724d
KurMalual <k.malual@alustudent.com>
2025-04-29 23:59:52 +0000

    done

--- Final_project/resume_classifier_app.py
+++ Final_project/resume_classifier_app.py
@@ -91,7 +91,7 @@ if uploaded_file and not file_processed:
     st.write(f"Prediction using Random Forest: {rf_pred[0]}")
 
 # Load the dataset for visualization
-df = pd.read_csv("C:\\Users\\majok\\Downloads\\ResumeDataSet.csv")
+df = pd.read_csv("C:\\Users\\majok\\Downloads\\Final_project\\ResumeDataSet.csv")
 
 # Clean the dataset
 df['cleaned_resume'] = df['Resume'].apply(clean_resume)
